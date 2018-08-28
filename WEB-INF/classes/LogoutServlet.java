830ca04d0b8abada600c290b5e3aa9d5b4a86b6f
nikitha <nikithamurugan27@gmail.com>
2018-08-27 17:41:44 -0700

    first commit

---
+++ WEB-INF/classes/LogoutServlet.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+public class LogoutServlet extends HttpServlet {  
+        protected void doGet(HttpServletRequest request, HttpServletResponse response)  
+                                throws ServletException, IOException {  
+            response.setContentType("text/html");  
+            PrintWriter out=response.getWriter();  
+              
+            request.getRequestDispatcher("link.html").include(request, response);  
+              
+            HttpSession session=request.getSession();  
+            session.invalidate();  
+              
+            out.print("You are successfully logged out!");  
+              
+            out.close();  
+    }  
+}  
